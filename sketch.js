c8d44c9101da81e134f4ed8fbec6c61bca897afa
nadia-mehjabin <95492048+nadia-mehjabin@users.noreply.github.com>
2021-12-03 11:46:14 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+let osc, playing, freq, amp;
+function setup() {
+  // put setup code here
+  let cnv= createCanvas(300,500);
+  
+  cnv.mousePressed(playOscillator);
+  osc = new p5.Oscillator('cosine');
+  
+  
+}
+function draw() {
+  // put drawing code here
+  background(155);
+  freq = constrain(map(mouseX, 0, width, 100, 500), 100, 500);
+  amp = constrain(map(mouseY, height, 0, 0, 1), 0, 1);
+
+  text('tap to play', 20, 20);
+  text('freq: ' + freq, 20, 40);
+  text('amp: ' + amp, 20, 60);
+if (playing) {
+    // smooth the transitions by 0.1 seconds
+    osc.freq(freq, 0.05);
+    osc.amp(amp, 0.05);
+  }
+}
+
+function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
+  osc.start();
+  playing = true;
+}
+
+function mouseReleased() {
+  // ramp amplitude to 0 over 0.5 seconds
+  osc.amp(0, 0.5);
+  playing = false;
+}
+  
